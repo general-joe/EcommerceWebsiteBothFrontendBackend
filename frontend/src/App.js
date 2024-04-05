a93d77f856f5a1025e6f0d4c0df9838a98058f38
D-General <kwabijoe274@gmail.com>
2024-04-05 19:41:19 +0000

    Added footer page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Shop from "./Pages/Shop";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignUp from "./Pages/LoginSignUp";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignUp />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
