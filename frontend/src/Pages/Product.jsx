0d6124b2061fb0626b92b469687b3e6065afd3c5
D-General <kwabijoe274@gmail.com>
2024-04-09 16:02:17 +0000

    Start with the cart page

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import Breadcrum from "../Components/Breadcrum/Breadcrum";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -12,8 +13,9 @@ const Product = () => {
   return (
     <div>
       <Breadcrum product={product} />
-      <ProductDisplay product={product}/>
-      <DescriptionBox/>
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 };
