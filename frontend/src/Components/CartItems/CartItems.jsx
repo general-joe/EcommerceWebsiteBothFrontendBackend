6973eff8842a6193309ad460fd496468621aba1f
D-General <kwabijoe274@gmail.com>
2024-04-10 16:52:18 +0000

    Added the cart page

--- frontend/src/Components/CartItems/CartItems.jsx
+++ frontend/src/Components/CartItems/CartItems.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import "./CartItems.css";
 import { ShopContext } from "../../Context/ShopContext";
 import remove_icon from "../Assets/cart_cross_icon.png";
+
 const CartItems = () => {
   const { all_product, CartItems, removeFromCart } = useContext(ShopContext);
   return (
@@ -15,9 +16,63 @@ const CartItems = () => {
         <p>Remove</p>
       </div>
       <hr />
-      {all_product.map((e)=>{
 
+      {all_product.map((e) => {
+        if (CartItems[e.id] > 0) {
+          return (
+            <div>
+              <div className="cartitems-format cartitems-format-main">
+                <img src={e.image} alt="" className="carticon-product-icon" />
+                <p>{e.name}</p>
+                <p>GHC{e.new_price}</p>
+                <button className="cartitems-quantity">
+                  {CartItems[e.id]}
+                </button>
+                <p>GHC{e.new_price * CartItems[e.id]}</p>
+                <img
+                  className="cartitems-remove-icon"
+                  src={remove_icon}
+                  onClick={() => {
+                    removeFromCart(e.id);
+                  }}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+        return null;
       })}
+      <div className="cartitems-down">
+        <div className="cartitems-total">
+          <h1>cart Totals</h1>
+          <div>
+            <div className="cartitems-total-item">
+              <p>Subtotal</p>
+              <p>GHC{0}</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <p>Delivery Fee</p>
+              <p>Free</p>
+            </div>
+            <hr />
+            <div className="cartitems-total-item">
+              <h3>Total</h3>
+              <h3>GHC{0}</h3>
+            </div>
+          </div>
+          <button>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cartitems-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cartitems-promobox">
+            <input type="text" placeholder="Promo code" />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
