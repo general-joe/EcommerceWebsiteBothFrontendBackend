4743c11c3e57a5628fac74ce5029ad8ea3e4d40d
lockwud <oklement3@gmail.com>
2024-04-23 15:31:58 +0000

    update in the route

--- Backend/routes/v1/web/clothes.js
+++ Backend/routes/v1/web/clothes.js
@@ -2,8 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 const clothes = require("../../../controllers/clothes")
+const upload = require("../../../utils/multerUtil");
 
-router.post("/save", clothes.saveClothes)
+router.post("/save",  upload.single("image"),clothes.saveClothes)
 router.get("/list", clothes.getClothes)
 router.get("/:id", clothes.getsingleClothe)
 router.patch("/:id", clothes.updateClothe)
--- Backend/routes/v1/web/index.js
+++ Backend/routes/v1/web/index.js
@@ -3,7 +3,6 @@ const route = Router();
 const client = require("./client");
 const collection = require("./collection");
 const order = require("./order");
-const sneakers = require("./sneakers");
 const clothes = require("./clothes");
 const delivery = require("./delivery");
 
@@ -12,7 +11,7 @@ route.use("/collection", collection);
 route.use("/delivery", delivery);
 route.use("/clothes", clothes);
 route.use("/order", order);
-route.use("/sneakers", sneakers);
+
 
 
 
