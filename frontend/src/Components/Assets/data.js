776ae4ffe133647036b1a93dc18ee72e8fbd901e
lockwud <oklement3@gmail.com>
2024-04-25 13:44:38 +0000

    updated Baya's picture

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -3,11 +3,11 @@ import member342 from "./member-342.jpg";
 import member343 from "./member-343.jpg";
 import member345 from "./member-345.jpg";
 // import member344 from "./member-344.jpg";
-// import member346 from "./member-346.jpg";
-import member349 from "./member-349.jpg";
-import member347 from "./member-347.jpg";
+import member346 from "./member-346.jpg";
+//import member349 from "./member-349.jpg";
+//import member347 from "./member-347.jpg";
 import member348 from "./member-348.jpg";
-import member350 from "./member-350.jpg";
+//import member350 from "./member-350.jpg";
 
 const members_data = [
   {
@@ -24,15 +24,26 @@ const members_data = [
     image: member345,
     indexNumber: "5221040345",
   },
-  { id: 8, name: "Rashid Bawa", image: member347, indexNumber: "5221040346" },
-  
+  { id: 8, name: "Rashid Bawa", image: member346, indexNumber: "5221040346" },
+  {
+    id: 5,
+    name: "Antwi Bosiako Kwame",
+    image: member347,
+    indexNumber: "5221040347",
+  },
   {
     id: 6,
     name: "Obeng Clement Boadu",
     image: member348,
     indexNumber: "5221040348",
   },
-
+  { id: 6, name: "Asuo Gdofred", image: member349, indexNumber: "5221040349" },
+  {
+    id: 7,
+    name: "Noamesi Edmund",
+    image: member350,
+    indexNumber: "5221040350",
+  },
 ];
 
 export default members_data;
