e5b54006bf2cc20c1fcea71e273f365d702c8cb6
Masood Acheampong <masoodacheampong@gmail.com>
2024-04-25 05:59:10 +0100

    Completed building the about us page and changed a few links on the footer

--- frontend/src/Components/Assets/data.js
+++ frontend/src/Components/Assets/data.js
@@ -4,7 +4,7 @@ import member343 from "./member-343.jpg";
 import member345 from "./member-345.jpg";
 // import member344 from "./member-344.jpg";
 // import member346 from "./member-346.jpg";
-// import member349 from "./member-349.jpg";
+import member349 from "./member-349.jpg";
 import member347 from "./member-347.jpg";
 import member348 from "./member-348.jpg";
 import member350 from "./member-350.jpg";
@@ -12,16 +12,38 @@ import member350 from "./member-350.jpg";
 const members_data = [
   {
     id: 1,
-    name: "",
+    name: "Ansere Richmond",
     image: member341,
     indexNumber: "5221040341",
   },
-  { id: 2, name: "", image: member342, indexNumber: "5221040342" },
-  { id: 3, name: "", image: member343, indexNumber: "5221040343" },
-  { id: 4, name: "", image: member345, indexNumber: "5221040345" },
-  { id: 5, name: "", image: member347, indexNumber: "5221040347" },
-  { id: 6, name: "", image: member348, indexNumber: "5221040348" },
-  { id: 7, name: "", image: member350, indexNumber: "5221040350" },
+  { id: 2, name: "Owusu Samuel", image: member342, indexNumber: "5221040342" },
+  { id: 3, name: "Antwi Clinton", image: member343, indexNumber: "5221040343" },
+  {
+    id: 4,
+    name: "Elizabeth Afrakomah",
+    image: member345,
+    indexNumber: "5221040345",
+  },
+  { id: 8, name: "Rashid Bawa", image: member347, indexNumber: "5221040346" },
+  {
+    id: 5,
+    name: "Antwi Bosiako Kwame",
+    image: member347,
+    indexNumber: "5221040347",
+  },
+  {
+    id: 6,
+    name: "Obeng Clement Boadu",
+    image: member348,
+    indexNumber: "5221040348",
+  },
+  { id: 6, name: "Asuo Gdofred", image: member349, indexNumber: "5221040349" },
+  {
+    id: 7,
+    name: "Noamesi Edmund",
+    image: member350,
+    indexNumber: "5221040350",
+  },
 ];
 
 export default members_data;
